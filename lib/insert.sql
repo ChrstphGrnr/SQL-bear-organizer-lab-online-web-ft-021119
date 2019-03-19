5527d1ecd982ca457c02675cfc6b2eb36af0e3bc
Christoph Gruener <christophgruener@me.com>
2019-03-19 15:02:12 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,5 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Winnie", 2, "male", "brown", "agressive", 1)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Winnie", 2, "male", "brown", "agressive", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Minnie", 1, "female", "light-brown", "mellow", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Fuzzy", 10, "male", "dark-brown", "easy-going", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Frank", 4, "male", "black", "depressed", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("", 2, "male", "brown", "agressive", 1);
