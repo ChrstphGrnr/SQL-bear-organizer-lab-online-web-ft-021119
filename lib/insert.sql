901db7c9f829941e391e03e72b8c534dffd2a92e
Christoph Gruener <christophgruener@me.com>
2019-03-19 15:03:48 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -2,4 +2,6 @@ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Winnie
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Minnie", 1, "female", "light-brown", "mellow", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Fuzzy", 10, "male", "dark-brown", "easy-going", 0);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Frank", 4, "male", "black", "depressed", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Polar", 7, "female", "white", "hungry", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Panda", 6, "female", "black/white", "transcending", 0);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("", 2, "male", "brown", "agressive", 1);
